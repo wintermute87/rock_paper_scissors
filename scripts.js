6d94dd1f1cdb3e20825945b0a5526532b90486de
wintermute87 <drimba.adrian@gmail.com>
2023-02-12 11:11:24 +0200

    Add function playround()

--- scripts.js
+++ scripts.js
@@ -22,7 +22,45 @@ function getComputerChoice()
 
 function playRound(playerSelection, computerSelection) 
 {
-   
+    switch (playerSelection.toLowerCase()) 
+    {
+        case "rock":
+        {
+            if(computerSelection === "scissors")
+                return "You win: Rock beats scissors";
+            else if(computerSelection === "paper")
+                return "You lose: paper beats rock";
+            else
+                return "Tie: rock";
+        }    
+            break;
+    
+        case "paper":
+        {
+            if(computerSelection === "rock")
+                return "You win: paper beats rock";
+            else if(computerSelection === "scissors")
+                return "You lose: scissors beats paper";
+            else
+                return "Tie: paper";
+        }    
+            break;
+
+        case "scissors":
+        {
+            if(computerSelection === "paper")
+                return "You win: scissors beats paper";
+            else if(computerSelection === "rock")
+                return "You lose: rock beats scissors";
+            else
+                return "Tie: scissors";
+        }    
+            break;
+
+
+        default:
+            break;
+    }
 }
    
 const playerSelection = "rock";
