b23b478b52c3d575dbd24b2628ef6294380137f5
wintermute87 <drimba.adrian@gmail.com>
2023-02-19 17:11:40 +0200

    Add UI interface

--- scripts.js
+++ scripts.js
@@ -63,31 +63,23 @@ function playRound(playerSelection, computerSelection)
     }
 }
 
-function game()
+
+
+
+
+function game(playerChoice)
 {
 
-    let yourScore = 0;
-    let computerScore = 0;
-
-    for (let i = 1; i < 6; i++) 
-    {
-        let playerSelection;
-        do {
-            playerSelection = prompt("Enter paper, rock or scissors:");
-        } while (playerSelection.toLowerCase() != "rock" && playerSelection.toLowerCase() != "paper" && playerSelection.toLowerCase() != "scissors");
         
+    //console.log(e.target);
 
-
-        let computerSelection = getComputerChoice();
-    
-        console.log(`********  Round ${i}  ********`)
-        console.log(`You played ${playerSelection}`);
-        console.log(`The computer played ${computerSelection}`);
-    
-        let result_round = playRound(playerSelection, computerSelection);
-        
+        let computer_choice = getComputerChoice();
+        result_round = playRound(playerChoice,computer_choice);
         console.log(result_round);
-        console.log(`******************************\n\n`)
+
+
+        div_result.textContent = result_round;
+        containerResultRound.appendChild(div_result);
 
         if(result_round.startsWith("You win"))
         {
@@ -97,22 +89,112 @@ function game()
         {
             computerScore ++;
         }
+
+        console.log(`your_score =  ${yourScore} `);
+        console.log(`computer_score =  ${computerScore} `);
+
+        if(yourScore === 5)
+        {
+            console.log(`Congratulations ! You won the game`);
+            document.querySelector('.rock').disabled = true;
+            document.querySelector('.paper').disabled = true;
+            document.querySelector('.scissors').disabled = true;
+            
+        //    yourScore=0;
+        //    computerScore=0;
+
+            gameResult.textContent = "Congratulations ! You won the game";
+            gameResultScore.appendChild(gameResult);
+        }    
+        
+        if(computerScore === 5)
+        {
+            console.log(`The computer won game`);
+            document.querySelector('.rock').disabled = true;
+            document.querySelector('.paper').disabled = true;
+            document.querySelector('.scissors').disabled = true;
+            
+            //    yourScore=0;
+         //   computerScore=0;
+
+            gameResult.textContent = "The computer won the game";
+            gameResultScore.appendChild(gameResult);
+
+
+         //   document.body.removeChild(gameResult);
+        }    
+
+        yourScoreResult.textContent = "Your score: " + yourScore;
+        containerResultScore.appendChild(yourScoreResult);
+
+
+        computerScoreResult.textContent = "The computer score: " + computerScore;
+        containerResultScore.appendChild(computerScoreResult);
+
+ 
+       
+    }
+   
+    function newGame()
+    {   
+        location.reload();
+        /*
+        if(document.querySelector(".content_result") != null)
+            document.body.removeChild(gameResult);
+        */
     }
 
+    let yourScore = 0;
+    let computerScore = 0;
+    let newGameStart = 0;
 
-    if(yourScore>computerScore)
-        console.log(`Congratulations ! You won ${yourScore} rounds of the game`);
-    else if(yourScore<computerScore)
-        console.log(`Computer won. Unfortunately you won only ${yourScore} rounds of the game`);
-    else
-        console.log(`Tie. Both you and the computer won ${yourScore} rounds`)
+
+    let result_round = "";
+
+
+    const container = document.querySelector(".results");
+    const containerResultRound = document.querySelector(".result-round");
+    const containerResultScore = document.querySelector(".result-score");
+    const gameResultScore = document.querySelector(".game-result");
+
+    const div_result = document.createElement('div');
+    div_result.classList.add('content_result');
+
+
+    //your score result
+    const yourScoreResult = document.createElement('div');
+    yourScoreResult.classList.add('content_result');
+
+
+
+    //computer score result
+    const computerScoreResult = document.createElement('div');
+    computerScoreResult.classList.add('content_result');
+
+    //Game result
+    const gameResult = document.createElement('div');
+    gameResult.classList.add('content_result');
+
+
+
+    const btn_rock = document.querySelector('.rock');
+    btn_rock.addEventListener('click', function () {
+        game("rock");
+    });
+
+
+    const btn_paper = document.querySelector('.paper');
+    btn_paper.addEventListener('click', () => { game("paper")} );
+
+
+    const btn_scissors = document.querySelector('.scissors');
+    btn_scissors.addEventListener('click', function () {
+        game("scissors");
+    });
+
+    const new_gameButton = document.querySelector('.new-game-button');
+    new_gameButton.addEventListener('click', newGame);
+
    
-}
-   
-
-game()
 
 
-
-
-//console.log(getComputerChoice());
